582137218e95ba9ed5aadf516e901aaa72f09aae
coderjolly <harmanjolly97@gmail.com>
2022-03-17 12:19:37 +0530

    completed pun function

--- utils.py
+++ utils.py
@@ -41,15 +41,11 @@ def translationEng2Pun():
     return output
 
 def translationPun2Eng():
-
-    data = readFile(os.path.join(SRC_DIR, "userinput_punjabi.txt"))
-    data = [i.strip() for i in data]
-    for i in range(len(data)):
-        data[i] = preprocess(data[i])
     
-    writeFile(os.path.join(SRC_DIR, "input_punjabi.txt"), data)
+    output_filepath = os.path.join(OP_DIR, "output_english.txt")
+    remove_file(output_filepath)
 
-    os.system("onmt_translate -model model/punjabi2english.pt -src static/ip_files/input_punjabi.txt -output static/op_files/output.txt")
+    os.system("onmt_translate -model model/punjabi2english.pt -src static/ip_files/input_punjabi.txt -output static/op_files/output_english.txt")
 
     output = readFile(os.path.join(OP_DIR, "output_english.txt"))
     # for x in output:
